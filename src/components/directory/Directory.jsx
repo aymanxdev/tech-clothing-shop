32ccb45662227ddef0de8c51669c1d6d92cafafe
aymanxdev <ayman.bacc@gmail.com>
2021-09-08 19:44:09 +0100

    broken homepage into smaller components, finished styling menuitemcomponents

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import MenuItem from "../menu-item/MenuItem";
+import { sections } from "./directory.data";
+import "./directory.styles.scss";
+
+export default class Directory extends Component {
+  constructor() {
+    super();
+
+    this.state = { sections };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
