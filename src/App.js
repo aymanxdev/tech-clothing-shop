21d4753734708015a442495c27eb711740cf2191
aymanxdev <ayman.bacc@gmail.com>
2021-09-11 03:33:56 +0100

    uadd shop page, collection preview, and collection item plus styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/HomePage";
-
-const hats = (props) => {
-  console.log(props);
-  return <div>Hats page</div>;
-};
+import ShopPage from "./pages/shop/ShopPage";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
