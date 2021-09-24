d58f74b546f4c656ba0ba10e0830f0c6f7e8b257
aymanxdev <ayman.bacc@gmail.com>
2021-09-24 23:08:34 +0100

    adding sreselectors,replaced cart selectors with memoized with memoized selectors

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -3,6 +3,7 @@ import "./cartDropdown.styles.scss";
 import CustomButton from "../form-button/CustomButton";
 import { connect } from "react-redux";
 import CartItem from "../cart-item/CartItem";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
@@ -17,7 +18,7 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 export default connect(mapStateToProps)(CartDropdown);
